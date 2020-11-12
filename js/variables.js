579b4cd3fa13f49391740df5023160f1aae55f88
Gurar <jozyk@inbox.lt>
2020-11-12 09:59:34 +0200

    commit

---
+++ js/variables.js
@@ -0,0 +1,102 @@
+//Kintamųjų inicijavimas
+
+/*  1. Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+        a. Po kiekvieno jų inicijavimo, išvesti į console*/
+
+const a = 5, b = 10, c = 4.5;
+console.log(a);
+console.log(b);
+console.log(c);
+
+/*  2. Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+        a. Po kiekvieno jų inicijavimo, išvesti į console*/
+
+const name = 'Jezy', surname = 'Kozlovski', github = 'Gurar';
+console.log(name);
+console.log(surname);
+console.log(github);
+
+/*  3. Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+        a. Po kiekvieno jų inicijavimo, išvesti į console*/
+
+const numbers = [1,4.5,6,100,-3], luckyNumbers = [13, 6, 9, 666, 7], points = [5, 8, 7, 9, 10];
+console.log(numbers);
+console.log(luckyNumbers);
+console.log(points);
+
+/*  4. Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+        a. Po kiekvieno jų inicijavimo, išvesti į console*/
+
+const citys = ['Vilnius', 'Kaunas', 'Panevezys', 'Siauliai', 'Klaipeda'],
+      fruits = ['apple', 'pineapple', 'banana', 'orange', 'strawberry'],
+      names = ['Tomas', 'Povilas', 'Anna', 'Jonas', 'Antanas'];
+console.log(citys);
+console.log(fruits);
+console.log(names);
+
+//Veiksmai su kintamaisiais
+
+/*  1. Susumuoti visus skaičiaus tipo kintamuosius
+        a. Rezultatą išvesti į console*/
+const sum = a + b + c;
+console.log(sum);
+
+/*  2. Sujungti visus teksto tipo kintamuosius taip, jog tarp jų būtų sudarytas tarpas
+        a. Rezultatą išvesti į console
+*/
+const text = `${name} ${surname} ${github}`;
+console.log(text);
+
+/*  3. Apskaičiuoti vertę iš sąrašų kurių verčių tipas yra skaičiai, pagal pateiktą logiką
+        a. 1-2+3-4+5
+        b. Rezultatą išvesti į console
+*/
+const sum2 = numbers[0] - numbers[1] + numbers[2] - numbers[3] + numbers[4];
+const sum3 = luckyNumbers[0] - luckyNumbers[1] + luckyNumbers[2] - luckyNumbers[3] + luckyNumbers[4];
+const sum4 = points[0] - points[1] + points[2] - points[3] + points[3];
+console.log(sum2);
+console.log(sum3);
+console.log(sum4);
+
+/*  4. Sujungti sąrašų vertes, kurių tipas yra tekstai, nuo sąrašo galo iki pradžios taip, jog tarp jų būtų kablelis ir tarpas*/
+
+const text2 = `${citys[4]}, ${citys[3]}, ${citys[2]}, ${citys[1]}, ${citys[0]}`;
+const text3 = `${fruits[4]}, ${fruits[3]}, ${fruits[2]}, ${fruits[1]}, ${fruits[0]}`;
+const text4 = `${names[4]}, ${names[3]}, ${names[2]}, ${names[1]}, ${names[0]}`;
+console.log(text2);
+console.log(text3);
+console.log(text4);
+
+//Kintamųjų palyginimas
+
+/*Lyginant, jei rezultatas tenkina palyginimo sąlygą, tai į console išvesti žodį “Pomidoras”, 
+o jei sąlyga nėra tenkinama, išvesti sakinį “Bandykite kitą kartą.”.*/
+
+/*  1. Tarpusavyje palyginti skaičiaus tipo kintamuosius:
+        a. kuris didesnis
+        b. kuris mažesnis
+        c. ar jie lygūs
+        d. ar jie nelygūs
+        e. kuris didesnis arba lygus
+        f. kuris mažesnis arba lygus
+*/
+
+
+if(b > a) {
+    console.log("Pomidoras");
+}else {
+    console.log("Bandykite kita karta");
+}
+
+/* Išvesti teksto tipo kintamųjų ilgius*/
+console.log(name.length);
+console.log(surname.length);
+console.log(github.length);
+
+
+
+
+
+
+
+
